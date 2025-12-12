9a4a14fd87d5f98f719f39558e124072e85e9f9f
Wiacek <benjamin.wiacek@gmail.com>
2025-12-11 22:33:30 -0500

    function addMovie
    Function to add movie to saved list and local storage. Render function needed next as well as clearing and reset.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const input = document.getElementById("input")
 const search = document.getElementById("search-form")
 const movieList = document.getElementById("movie-list")
+const myMovieList = document.getElementById("my-movie-list")
 
-search.addEventListener("submit", getMovieList)
-let moviesArr = []
-let listMovies = ""
+let moviesResultsArr = []
+let listMoviesResults = ""
 
-async function getMovieList(event) {
-    event.preventDefault();
-    moviesArr = []
-    listMovies = ""
+let myMoviesArr = JSON.parse(localStorage.getItem("My movie list")) || []
+let myMoviesHTML = ""
+
+if (search) {
+    search.addEventListener("submit", getMovieList)
+}
+
+async function getMovieList(e) {
+    e.preventDefault();
+    moviesResultsArr = []
+    listMoviesResults = ""
     const res = await fetch(`http://www.omdbapi.com/?apikey=8a8e1701&s=${input.value}&type=movie&r=json`)
     const data = await res.json()
     console.log(data)
@@ -17,11 +24,11 @@ async function getMovieList(event) {
         for(const result of data.Search) {
             const movieRes = await fetch(`http://www.omdbapi.com/?apikey=8a8e1701&i=${result.imdbID}&r=json`)
             const movieData = await movieRes.json()
-            moviesArr.push(movieData)
+            moviesResultsArr.push(movieData)
         }
-        console.log(moviesArr)
-        for(const movie of moviesArr) {
-            listMovies += 
+
+        for(const movie of moviesResultsArr) {
+            listMoviesResults += 
             `<div class="movie">
                 <img src="${movie.Poster} alt="${movie.Title} poster" class="poster">
                 <div class="movie-block">
@@ -32,7 +39,7 @@ async function getMovieList(event) {
                     <div class="movie-details">
                         <p class="runtime">${movie.Runtime}</p> 
                         <p class="genre">${movie.Genre}</p>
-                        <div class="add-movie" id="add-movie">
+                        <div class="add-movie" data-id="${movie.imdbID}">
                             <img class="plus-icon" src="/images/plus-icon.png">
                             <span>Watchlist</span>
                         </div>
@@ -42,54 +49,56 @@ async function getMovieList(event) {
             </div>`
         }
     } else {
-        listMovies = `<div class="error-text">Unable to find what you’re looking for. Please try another search.</div>`
+        listMoviesResults = `<div class="error-text">Unable to find what you’re looking for. Please try another search.</div>`
     }
 
-    movieList.innerHTML = listMovies
+    movieList.innerHTML = listMoviesResults
 }
 
-// {
-//     "Title": "The Prestige",
-//     "Year": "2006",
-//     "Rated": "PG-13",
-//     "Released": "20 Oct 2006",
-//     "Runtime": "130 min",
-//     "Genre": "Drama, Mystery, Sci-Fi",
-//     "Director": "Christopher Nolan",
-//     "Writer": "Jonathan Nolan, Christopher Nolan, Christopher Priest",
-//     "Actors": "Christian Bale, Hugh Jackman, Scarlett Johansson",
-//     "Plot": "Rival 19th-century magicians engage in a bitter battle for trade secrets.",
-//     "Language": "English",
-//     "Country": "United Kingdom, United States",
-//     "Awards": "Nominated for 2 Oscars. 6 wins & 44 nominations total",
-//     "Poster": "https://m.media-amazon.com/images/M/MV5BMTM3MzQ5MjQ5OF5BMl5BanBnXkFtZTcwMTQ3NzMzMw@@._V1_SX300.jpg",
-//     "Ratings": [
-//         {
-//             "Source": "Internet Movie Database",
-//             "Value": "8.5/10"
-//         },
-//         {
-//             "Source": "Rotten Tomatoes",
-//             "Value": "77%"
-//         },
-//         {
-//             "Source": "Metacritic",
-//             "Value": "66/100"
-//         }
-//     ],
-//     "Metascore": "66",
-//     "imdbRating": "8.5",
-//     "imdbVotes": "1,538,383",
-//     "imdbID": "tt0482571",
-//     "Type": "movie",
-//     "DVD": "N/A",
-//     "BoxOffice": "$53,089,891",
-//     "Production": "N/A",
-//     "Website": "N/A",
-//     "Response": "True"
+if (movieList) {
+    movieList.addEventListener("click", function(e) {
+        const addMovieBtn = e.target.closest(".add-movie")
+        if(addMovieBtn && addMovieBtn.dataset.id) {
+            addMyMovie(addMovieBtn.dataset.id)
+        }
+    })
+}
+
+async function addMyMovie(filmId) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=8a8e1701&i=${filmId}&r=json`)
+    const movie = await res.json()
+    if (!myMoviesArr.includes(movie)) {
+        myMoviesArr.push(movie)
+    }
+    console.log("movie-added")
+    console.log(myMoviesArr)
+    localStorage.setItem("My movie list", JSON.stringify(myMoviesArr))
+}
+
+// function renderMyList() {
+//     for(const movie of myMoviesArr) {
+//         myMoviesHTML =
+//         `<div class="movie">
+//             <img src="${movie.Poster} alt="${movie.Title} poster" class="poster">
+//             <div class="movie-block">
+//                 <div class="movie-headline">
+//                     <h3 class="movie-title">${movie.Title}</h3>
+//                     <a href="https://www.imdb.com/title/${movie.imdbID}" class="rating"><img class="star-icon" src="/images/icon-star.png">${movie.imdbRating}</a>
+//                 </div>
+//                 <div class="movie-details">
+//                     <p class="runtime">${movie.Runtime}</p> 
+//                     <p class="genre">${movie.Genre}</p>
+//                     <div class="add-movie" data-id="${movie.imdbID}">
+//                         <img class="plus-icon" src="/images/plus-icon.png">
+//                         <span>Watchlist</span>
+//                     </div>
+//                 </div>
+//                 <p class="movie-plot">${movie.Plot}</p>
+//             </div>
+//         </div>`
+//     }
+//     myMovieList.innerHTML = myMoviesHTML
 // }
 
-// {
-//     "Response": "False",
-//     "Error": "Movie not found!"
-// }
+// renderMyList()
+// console.log(myMoviesArr)
